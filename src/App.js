53d4c5062d1cc1d1c6ad3fb0338fb86d0decfcf0
hermit-github <shubhamhalder0@gmail.com>
2023-06-24 19:34:42 +0530

    useEffect Cleaner Function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState([])
+  const [id, setId] = useState(1)
+
+  useEffect(() =>{
+    let subs = true
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      if(subs){
+        setUser(data)
+      }
+    })
+
+    return () => {
+      subs = false
+    }
+  },[id])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="" style={{display:"flex",gap:'10px'}}>
+        <button onClick={() => setId(1)}>Id 1</button>
+        <button onClick={() => setId(2)}>Id 2</button>
+        <button onClick={() => setId(3)}>Id 3</button>
+      </div>
+      
+      <div className="user">
+        <p>Name:{user.name}</p>
+        <p>Username:{user.username}</p>
+        <p>Email:{user.email}</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
